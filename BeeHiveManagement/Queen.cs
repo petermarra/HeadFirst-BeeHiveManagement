6b8cf2688d113524bcecc60fa29c20da37252b42
Peter Marra <neverquit@roadrunner.com>
2020-08-24 16:24:57 -0400

    My version

---
+++ BeeHiveManagement/Queen.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeHiveManagement
+{
+    class Queen
+    {
+        Worker[] workers;
+        int shiftNumber;
+
+        public Queen(Worker[] workers)
+        {
+            this.workers = workers;
+        }
+        public bool AssignWork(string job, int shift)
+        {
+            for (int i = 0; i < workers.Length; i++)
+            {
+                if (workers[i].DoThisJob(job,shift))
+                {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+        public string WorkTheNextShift()
+        {
+            shiftNumber += 1;
+            string report = $"Report for shift {shiftNumber}\r\n";
+            for (int i = 0; i < workers.Length; i++)
+            {
+                if (string.IsNullOrEmpty(workers[i].CurrentJob))
+                    report += $"Worker #{i+1} is not working\r\n";
+                else
+                {
+                    if (workers[i].DidYouFinish())
+                    {
+                        report += $"Worker #{i+1} finished the job\r\n";
+                        report += $"Worker #{i+1} is not working\r\n";
+                    }
+                    else
+                    {
+                        if (workers[i].ShiftsLeft == 1)
+                            report += $"Worker #{i+1} will be done with {workers[i].CurrentJob} after this shift\r\n";
+                        else
+                            report += $"Worker #{i+1} is doing {workers[i].CurrentJob} for {workers[i].ShiftsLeft} more shifts\r\n";
+                    }
+                }
+            }
+            return report;
+
+        }
+    }
+}
---
+++ BeeHiveManagement/Worker.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeHiveManagement
+{
+    class Worker
+    {
+        string currentJob;
+        public string CurrentJob
+        {
+            get
+            {
+                return currentJob;
+            }
+        }
+
+        int shiftsLeft;
+        public int ShiftsLeft
+        {
+            get
+            {
+                return shiftsLeft;
+            }
+        }
+
+        string[] jobsICanDo;
+        private int shiftsToWork;
+        int shiftsWorked;
+
+        public Worker(string[] jobsICanDo)
+        {
+            this.jobsICanDo = jobsICanDo;  
+        }
+        public bool DoThisJob(string jobToDo, int shiftsToWork)
+        {
+            if (!string.IsNullOrEmpty(CurrentJob))
+            {
+                return false;
+            }
+
+            for (int i = 0; i < jobsICanDo.Length; i++)
+                {
+                if (jobsICanDo[i]==jobToDo)
+                {
+                    currentJob = jobToDo;
+                    shiftsLeft = shiftsToWork;
+                    this.shiftsToWork = shiftsToWork;
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        public bool DidYouFinish()
+        {
+            shiftsLeft -=1;
+            shiftsWorked += 1;
+
+            if (shiftsLeft == 0)
+            {
+                currentJob = "";
+                return true;
+            }
+            return false;
+        }
+    }
+}
